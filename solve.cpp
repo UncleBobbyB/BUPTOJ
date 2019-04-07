06f44872837d4174f5c0b810d1c0832160567b73
UncleBobbyB <jyinj@outlook.com>
2019-04-07 20:16:30 +0800

    This is the template i've been using for like, when i'm playing around with ACM, though i fucked up:(

---
+++ solve.cpp
@@ -0,0 +1,78 @@
+#include<bitset>
+#include<cassert>
+#include<string>
+#include<sstream>
+#include<stack>
+#include<queue>
+#include<cctype>
+#include<algorithm>
+#include<map>
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<cstring>
+#include<vector>
+#include<deque>
+#include<algorithm>
+#include<set>
+// #include<bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define pli pair<LL, int>
+#define pil pair<int, LL>
+#define pll pair<LL, LL>
+#define vi vector<int>
+#define LL long long
+#define INF 1e9
+#define DINF 1e30
+#define LLINF 1e18
+#define ULL unsigned long long
+#define UI unsigned int
+#define eps 1e-9
+
+//#include<cmath>
+//#define PI acos(-1.0)
+
+#define all(x) (x).begin(), (x).end()
+#define ue(x) (x).erase(unique(all((x))), (x).end())
+
+void err() {
+  cout << "shit" << endl;
+  exit(1);
+}
+
+
+int main() {
+#ifdef LOCAL
+	freopen("in", "r", stdin);
+#endif
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
